c84688e436320ec1335afb17e4226d3f28a4d4c0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-12-01 21:36:53 +0900

    example ts 5

--- src/exampleTS.ts
+++ src/exampleTS.ts
@@ -1,12 +1,5 @@
 // @ts-ignore
-let count: number;
-count = 1;
+let mine = {}
+mine = {name: "None"}
 
-let strig: string;
-// @ts-ignore
-const non = strig.length
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// @ts-ignore
-let test = ['none', "balap", "typescript", 55];
-test.push(11)
+mine.address = "Where"
